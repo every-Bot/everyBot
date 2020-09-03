041610cc45acfebd8ad28cb0d536db0c12f3e305
Joel Giovinazzo <joelgiovinazzo@gmail.com>
2020-09-03 11:29:17 +0000

    Clean up dnd

--- everyBot/cogs/dnd/dnd.py
+++ everyBot/cogs/dnd/dnd.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import has_permissions
+from textwrap import dedent
 
 import random
 
@@ -8,7 +9,7 @@ import random
 async def check_disabled(ctx):
     return ctx.command.name not in ctx.bot.disabled_commands
 
-class DnD(commands.Cog, name='DnD'):
+class DnD(commands.Cog, name='dnd'):
     def __init__(self, bot):
         self.bot = bot
 
@@ -16,14 +17,27 @@ class DnD(commands.Cog, name='DnD'):
     @commands.command()
     @commands.check(check_disabled)
     async def rollto(self, ctx, *, text):
-        return await ctx.send(f"{ ctx.author.mention } rolled to { text } and got a { random.randint(0, 20) }!")
+        embed = discord.Embed(
+            title="Roll Results",
+            colour=discord.Color.blue(),
+            description=f"{ ctx.author.mention } rolled to { text } and got a { random.randint(0, 20) }!"
+        )
+
+        return await ctx.send(embed=embed)
 
     """ Error Check """
     async def cog_command_error(self, ctx, error):
         # Handling any errors within commands
-        if 'The check functions for command' in str(error):
-            return
-        await ctx.send(f'Error in { ctx.command.qualified_name }: { error }')
+        embed = discord.Embed(
+            title=f"Error in { ctx.command.qualified_name }",
+            colour=discord.Color.red(),
+            description=dedent(f"""
+                { error }
+                Use `{ self.bot.command_prefix }help { ctx.command.qualified_name }` for help with the command.
+                """)
+        )
+
+        return await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(DnD(bot))
