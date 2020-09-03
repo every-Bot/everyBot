f62ca70172a00a36c1998075bef9095e4843005d
Joel Giovinazzo <joelgiovinazzo@gmail.com>
2020-09-03 11:24:43 +0000

    Clean up animals

--- everyBot/cogs/animals/animals.py
+++ everyBot/cogs/animals/animals.py
@@ -17,12 +17,11 @@ class Animals(commands.Cog, name='Animals'):
             # Get requested image
             res = requests.get(url)
             json_data = json.loads(res.text)
-        except Exception as e:
-            # Handle errors if any
-            await ctx.send(f'**`ERROR:`** { type(e).__name__ } - { e }')
-        else:
-            # Return data in json format
-            return json_data
+        except requests.exceptions.RequestException as e:
+            raise e
+            
+        # Return data in json format
+        return json_data
 
 
     """ Cats """
@@ -30,45 +29,99 @@ class Animals(commands.Cog, name='Animals'):
     @commands.check(check_disabled)
     @commands.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
     async def cat(self, ctx):
-        response = await self.get_image(ctx, 'https://api.alexflipnote.dev/cats', 'file')
-        await ctx.send(response['file'])
+        try:
+            response = await self.get_image(ctx, 'https://api.alexflipnote.dev/cats', 'file')
+        except requests.exceptions.RequestException as e:
+            embed = discord.Embed(
+                title=f"Request Error: { type(e).__name__ }",
+                colour=discord.Color.red(),
+                description=f"{ e }"
+            )
+            return await ctx.send(embed=embed)
+        
+        return await ctx.send(response['file'])
 
     """ Sad Cat """
     @commands.command(aliases=['sadkitty'])
     @commands.check(check_disabled)
     @commands.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
     async def sadcat(self, ctx):
-        response = await self.get_image(ctx, 'https://api.alexflipnote.dev/sadcat', 'file')
-        await ctx.send(response['file'])
+        try:
+            response = await self.get_image(ctx, 'https://api.alexflipnote.dev/sadcat', 'file')
+        except requests.exceptions.RequestException as e:
+            embed = discord.Embed(
+                title=f"Request Error: { type(e).__name__ }",
+                colour=discord.Color.red(),
+                description=f"{ e }"
+            )
+            return await ctx.send(embed=embed)
+
+        return await ctx.send(response['file'])
 
     """ Dogs """
     @commands.command(aliases=['doggo', 'puppy', 'pupper', 'woofer'])
     @commands.check(check_disabled)
     @commands.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
     async def dog(self, ctx):
-        response = await self.get_image(ctx, 'https://api.alexflipnote.dev/dogs', 'file')
-        await ctx.send(response['file'])
+        try:
+            response = await self.get_image(ctx, 'https://api.alexflipnote.dev/dogs', 'file')
+        except requests.exceptions.RequestException as e:
+            embed = discord.Embed(
+                title=f"Request Error: { type(e).__name__ }",
+                colour=discord.Color.red(),
+                description=f"{ e }"
+            )
+            return await ctx.send(embed=embed)
+
+        return await ctx.send(response['file'])
 
     """ Birds """
     @commands.command(aliases=['birb', 'birdy'])
     @commands.check(check_disabled)
     @commands.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
     async def bird(self, ctx):
-        response = await self.get_image(ctx, 'https://api.alexflipnote.dev/birb', 'file')
-        await ctx.send(response['file'])
+        try:
+            response = await self.get_image(ctx, 'https://api.alexflipnote.dev/birb', 'file')
+        except requests.exceptions.RequestException as e:
+            embed = discord.Embed(
+                title=f"Request Error: { type(e).__name__ }",
+                colour=discord.Color.red(),
+                description=f"{ e }"
+            )
+            return await ctx.send(embed=embed)
+
+        return await ctx.send(response['file'])
 
     """ Duck """
     @commands.command(aliases=['ducky', 'duckling'])
     @commands.check(check_disabled)
     @commands.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
     async def duck(self, ctx):
-        response = await self.get_image(ctx, 'https://random-d.uk/api/v1/random', 'url')
-        await ctx.send(response['url'])
+        try:
+            response = await self.get_image(ctx, 'https://random-d.uk/api/v1/random', 'url')
+        except requests.exceptions.RequestException as e:
+            embed = discord.Embed(
+                title=f"Request Error: { type(e).__name__ }",
+                colour=discord.Color.red(),
+                description=f"{ e }"
+            )
+            return await ctx.send(embed=embed)
+
+        return await ctx.send(response['url'])
 
     """ Error Check """
     async def cog_command_error(self, ctx, error):
         # Handling any errors within commands
-        await ctx.send(f'Error in { ctx.command.qualified_name }: { error }')
+        embed = discord.Embed(
+            title=f"Error in { ctx.command.qualified_name }",
+            colour=discord.Color.red(),
+            description=dedent(f"""
+                { error }
+                Use `{ self.bot.command_prefix }help { ctx.command.qualified_name }` for help with the command.
+                """)
+        )
+
+        return await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Animals(bot))
