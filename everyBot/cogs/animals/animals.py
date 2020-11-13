e6f1457453a1e5599cf56a0d838ca918776f44d3
Joel Giovinazzo <joelgiovinazzo@gmail.com>
2020-11-13 10:43:20 +0000

    Add animal fact command

--- everyBot/cogs/animals/animals.py
+++ everyBot/cogs/animals/animals.py
@@ -151,6 +151,45 @@ class Animals(commands.Cog, name='animals'):
 
         return await ctx.send(response['url'])
 
+    """ Animal Fact """
+    @commands.command()
+    @commands.check(check_disabled)
+    @commands.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
+    async def animal_fact(self, ctx, animal: str):
+        animal_list = ["dog", "cat", "panda", "fox", "red_panda", "koala", "birb", "racoon", "kangaroo"]
+
+        # Ensure given animal is in list of accepted animals
+        if animal not in animal_list:
+            animals = ", ".join(animal_list)
+            embed = discord.Embed(
+                title="Amimal Fact",
+                colour=discord.Color.red(),
+                description=f"Please specify an animal in { animals }"
+            )
+            return await ctx.send(embed=embed)
+
+        # Make request
+        url = f"https://some-random-api.ml/animal/{ animal }"
+        try:
+            res = requests.get(url)
+            json_data = res.json()
+        except requests.exceptions.RequestException as e:
+            embed = discord.Embed(
+                title=f"Request Error: { type(e).__name__ }",
+                colour=discord.Color.red(),
+                description=f"{ e }"
+            )
+            return await ctx.send(embed=embed)
+
+        embed = discord.Embed(
+            title=f"{ animal.capitalize() } Fact",
+            colour=discord.Color.blue(),
+            description=f"{ json_data['fact'] }"
+        )
+        embed.set_thumbnail(url=json_data['image'])
+
+        return await ctx.send(embed=embed)
+
     """ Error Check """
     async def cog_command_error(self, ctx, error):
         # Handling any errors within commands
